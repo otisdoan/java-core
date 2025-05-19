c6a8681758e5ee5e5f5627b90cd33c3c2d53d27d
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2025-05-19 22:55:46 +0700

    update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,28 +1,13 @@
 import java.util.*;
 
-class Animal {
-    void sound() {
-        System.out.println("Animal sound");
-    }
-}
-
-class Dog extends Animal {
-    void bark() {
-        System.out.println("Woof!");
-    }
-}
-
 public class App {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("A");
-        list.add("Z");
-        list.add("C");
-        List<String> list1 = new ArrayList<>();
-        list1.add("D");
-        list1.add("E");
-        list.addAll(list1);
-        list.sort(String::compareTo);
-        System.out.println(list);
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("A");
+        hashSet.add("B");
+        hashSet.add("C");
+        hashSet.add(null);
+        hashSet.add("Le Doan Hieu");
+        System.out.println(hashSet);
     }
 }
